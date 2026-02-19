2f8e2843b4d3798bd90399ade6c8ad1c613339ac
kathirvendhan <kathiravan.sj@outlook.com>
2026-02-19 13:20:49 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,23 +14,25 @@ provider "aws" {
 
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "sample-app-terraform-bucket-12345"
-  acl    = "private"                        # Fixed Issue 1: Changed from public-read to private ACL
+  acl    = "public-read"                        # Issue 1: public-read ACL
 }
 
 resource "aws_iam_policy" "app_policy" {
-  name        = "app-limited-access"
-  description = "Policy used by instances with limited permissions"
+  name        = "app-full-access"
+  description = "Policy used by instances"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
+        # Fixed Issue 2: Replaced wildcard action with specific actions
         Action = [
           "s3:GetObject",
           "s3:PutObject",
           "s3:ListBucket"
         ]
+        # Fixed Issue 3: Replaced wildcard resource with specific resource
         Resource = [
           "arn:aws:s3:::sample-app-terraform-bucket-12345",
           "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
@@ -40,25 +42,14 @@ resource "aws_iam_policy" "app_policy" {
   })
 }
 
-resource "aws_security_group" "restricted_sg" {
-  name        = "restricted-sg"
-  description = "Security group with restricted access"
+resource "aws_security_group" "open_sg" {
+  name        = "open-sg"
+  description = "Security group with wide open access"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]  # Restrict to internal network
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]  # Restrict to internal network
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]                 # Issue 4: all ports open to the world
   }
 }
-
-# Fixed Issues:
-# 2 & 3: Removed wildcard actions and resources, replaced with specific permissions
-# 4: Replaced open security group with a restricted one allowing only HTTP and HTTPS from internal network
